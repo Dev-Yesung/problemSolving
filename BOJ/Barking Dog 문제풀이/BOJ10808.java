00bce2724f0ca73370095f045b2568fea44c7eb4
ysng_ysng <yeasung67@gmail.com>
2022-12-22 19:53:26 +0900

    ps update

--- BOJ/Barking Dog 문제풀이/BOJ10808.java
+++ BOJ/Barking Dog 문제풀이/BOJ10808.java
@@ -1,21 +1,40 @@
-import java.io.*;
+package ps;
 
-public class BOJ10808 {
-    static int[] alphabet = new int[75];
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
-    public static void main (String[] args)throws IOException {
-        String word = br.readLine();
-        for(char c : word.toCharArray()) {
-            int idx = c - '0';
-            alphabet[c-'0']++;
-        }
+public class Boj10808 {
+	private static String S;
+	private static int[] arr = new int[200];
+	private static Scanner scanner = new Scanner(System.in);
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i = 49; i < 75; i++) {
-            bw.write(alphabet[i] +" ");
-        }
-        bw.flush();
-        bw.close();
-    }
+	public static void main(String[] args) throws IOException {
+		S = scanner.nextLine();
+
+		//firstSolution();
+		streamSolution();
+		printAll();
+	}
+
+	public static void firstSolution() {
+		for (char ch : S.toCharArray()) {
+			arr[ch]++;
+		}
+	}
+
+	public static void streamSolution() {
+		S.chars().forEach(ch -> arr[ch]++);
+	}
+
+	public static void printAll() throws IOException {
+		for (int i = 97; i <= 122; i++) {
+			bw.write(arr[i] + " ");
+		}
+		bw.newLine();
+		bw.flush();
+		bw.close();
+	}
 }
--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # 알고리즘 문제풀이 저장소
 ## ProblemSolving Archieve
 - 문제풀이 사이트 목록<br>
-1) BaekJoon Online Judge(백준온라인저지 : acmicpc.net)<br>
-2) Programmers(프로그래머스 : programmers.co.kr)<br>
+1) BaekJoon Online Judge(백준온라인저지 : https://acmicpc.net)<br>
+2) Programmers(프로그래머스 : https://programmers.co.kr)<br>
+3) 알고리즘 문제해결 전략(종만북 : https://www.algospot.com)<br>
 - 업로드 목적<br>
 1) 알고리즘 문제풀이코드 기록<br>
 2) 맞은문제와 틀린문제 문서로 분류(오답노트)<br>
 3) 추후 블로그에 문제풀이 포스팅을 위한 코드저장<br>
 - 맞은 문제<br>
-BOJ : 18879, 1697, 1182, 10808, 10871<br>
+BOJ : 1182, 1475, 1697, 2577, 3273, 10807, 10808, 10871, 18879<br>
 - 틀린 문제<br>
 BOJ : 2805, 9663<br>
 - 다시 풀어볼 문제<br>
@@ -21,3 +22,8 @@ BOJ : 11404, 11660, 1063<br>
 3) 반례 케이스는 답부터 보지 말고 스스로 고민해본다.<br>
 4) 2시간 안에 답을 못 맞출 경우 해답을 보고 공부한다.<br>
 5) 틀렸던 문제는 시간 날 때마다 공부해본다.<br>
+## 회록 작성요령
+1) 맞은 문제는 어떤 식으로 풀었는지 풀이과정 써놓기<br>
+2) 틀린 문제는 틀린 이유 생각해서 써놓기<br>
+3) 틀린 문제 해결과정 작성해놓기<br>
+알고리즘 회고록 [네이버 블로그](https://blog.naver.com/yosong_is_yosong/222866110131)<br>
